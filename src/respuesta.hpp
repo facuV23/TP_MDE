867b3bde3d82ef97ef053d4519f9abb290ed6ef4
Facundo Viacava <facundoviacava@gmail.com>
2022-06-19 19:29:39 -0300

    agg mde comunicacion y respuesta

---
+++ src/respuesta.hpp
@@ -0,0 +1,13 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <string>
+
+namespace respuesta{
+
+void hola();
+void error();
+void Ok();
+bool reconocer(string resp);
+bool pass(string resp);
+
+}
