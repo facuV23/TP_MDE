e2a133b99e553ff1193162f5a33ca43df1eff914
Facundo Viacava <facundoviacava@gmail.com>
2022-06-19 19:30:18 -0300

    agg mde comunicacion y respuesta coreccion

--- src/respuesta.hpp
+++ src/respuesta.hpp
@@ -4,10 +4,11 @@
 
 namespace respuesta{
 
-void hola();
-void error();
-void Ok();
+void hola(string resp);
+void error(string resp);
+void ok(string resp);
 bool reconocer(string resp);
 bool pass(string resp);
+void init(string resp);
 
 }
